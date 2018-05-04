975da44535f4270be29a84f64a6549ac21a8233e
LeMMik <solopubli@live.com.ar>
2018-05-04 10:11:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ bot.on('message', (message) => {
    'TET: +45 (max 90)' + '\n' + 
    'PEN: max 124');
  }
- if (message.author = 'Mikel(LeMMik)'){
-  message.channel.sendMessage('Guapeton');
- }
- if (message.content == '!fs joyeria amarilla' || message.content == '!fs joyeria boss' || message.content == '!fs yellow accessories') {
+ if (message.content == '!fs joyeria amarilla' || message.content == '!fs joyeria boss' || message.content == '!fs yellow accessories' && message.author = 'Mikel(LeMMik)') {
   message.channel.sendMessage('PRI: 25 (max 25)' + '\n' + 
    'DUO: 35 (max 35)' + '\n' + 
    'TRI: 45 (max 45)' + '\n' + 
